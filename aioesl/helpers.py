f7f8880e935e29a07590e7025fbbc1b5e6a3a9df
alexey_panyovin <a@runtel.ru>
2016-06-15 09:31:12 +0300

    update helpers

--- aioesl/helpers.py
+++ aioesl/helpers.py
@@ -1,3 +1,7 @@
+from lxml import etree
+from .log import aioesl_log
+
+
 def print_event(event, filter=None, print_func=None):
     if event is None or event == {}:
         return
@@ -13,3 +17,42 @@ def print_event(event, filter=None, print_func=None):
         else:
             if str(filter) in str(k) or str(filter) in str(event[k]):
                 outer("%s: %s" % (k, event[k]))
+
+
+def parse_raw_split(split="|", raw="", need_fields=[], kill_fl=False, logger=aioesl_log):
+    data = []
+    try:
+        raw = raw.get("DataResponse")
+        if raw is None:
+            return data
+    except Exception as error:
+        logger.exception(error)
+        return None
+
+    lines = raw.splitlines()
+    if kill_fl and len(lines) > 1:
+        lines = lines[1:]
+
+    if len(lines) > 1:
+        keys = lines[0].strip().split(split)
+        for line in lines[1:]:
+            fields = line.split(split)
+            if len(fields) != len(keys):
+                continue
+            r = {keys[i]: fields[i] for i in range(0, len(keys)) if keys[i] in need_fields or len(need_fields) == 0}
+            data.append(r)
+
+    return data
+
+
+def parse_raw_xml(raw, logger=aioesl_log):
+    in_str_xml = raw.get("DataResponse")
+    if in_str_xml is None:
+        return None
+
+    try:
+        xml = etree.fromstring(in_str_xml)
+        return xml
+    except Exception as error:
+        logger.exception(error)
+        return None
